22a79e83676cd0b8767b2844f74afbfc5dd18ee2
Neel Gopaul <neelmedhanshgopaul@gmail.com>
2020-01-04 17:48:30 +0400

    Initial commit

---
+++ src/Controller/CustomPageController.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\custom_page\Controller\CustomPageController.
+ */
+
+namespace Drupal\custom_page\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class CustomPageController extends ControllerBase {
+  public function content() {
+
+    return [
+      '#theme' => 'my_template',
+      '#test_var' => $this->t('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+    ];
+
+  }
+}
